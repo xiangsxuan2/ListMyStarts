d3a2db7c5b15d19f6918d8b627623e8bef848722
xiangsxuan2025 <xiangshouxuan@foxmail.com>
2025-12-04 00:41:04 +0800

    fix: 生成文件放在全局根目录

--- ListMyStarts/Program.cs
+++ ListMyStarts/Program.cs
@@ -1,4 +1,5 @@
 using Octokit;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 // 传送了名字,否则默认
@@ -13,7 +14,7 @@ var github = new GitHubClient(new ProductHeaderValue("ListMyStarts"));
 var user = await github.User.Get(username);
 var starred = await github.Activity.Starring.GetAllForUser(username);
 
-// 生成md文件
+// 生成文件内容
 StringBuilder mdStr = new StringBuilder();
 int idx = 1;
 var title = $"{username}'s Starts";
@@ -25,8 +26,19 @@ foreach (var item in starred)
     mdStr.Append("]\n");
     idx++;
 }
-File.WriteAllText($"{title}.md", mdStr.ToString()
+
+// 写入到源码目录的md文件
+var path = GetFilePath();// 获取当前源码文件路径
+var dir = Path.GetDirectoryName(Path.GetDirectoryName(path));
+var mdfile = Path.Combine(dir, "My Starts.md");//跳转两层目录找到源代码里的md文件,这方法可能不通用
+File.WriteAllText(mdfile, mdStr?.ToString() ?? ""
                 , new UTF8Encoding(false)); // UTF8默认包含Bom, 会在开头多个/FEFF标志,
                                             // 影响md解析, 构造传false取消这个标志
 
-Console.WriteLine(mdStr);
+Console.WriteLine(mdStr?.ToString() ?? "");
+
+//调用源码路径
+static string GetFilePath([CallerFilePath] string path = null)
+{
+    return path;
+}
