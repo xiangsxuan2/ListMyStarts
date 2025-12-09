075abccf212635988d8968af75fee1188a624f37
xiangsxuan2025 <xiangshouxuan@foxmail.com>
2025-12-09 15:07:36 +0800

    feat: 增加个人仓库罗列功能

--- ListMyStarts/Program.cs
+++ ListMyStarts/Program.cs
@@ -9,31 +9,50 @@ if (args is not null && args.Length != 0)
     username = args[0];
 }
 
-// 获取用户star列表
+var projectDir = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
+if (string.IsNullOrWhiteSpace(projectDir))
+    throw new Exception("无法获取文件目录, 项目编译输出目录结构可能进行了调整");
+
 var github = new GitHubClient(new ProductHeaderValue("ListMyStarts"));
-var user = await github.User.Get(username);
-var starred = await github.Activity.Starring.GetAllForUser(username);
 
-// 生成文件内容
-StringBuilder mdStr = new StringBuilder();
-int idx = 1;
-var title = $"{username}'s Starts";
-mdStr.AppendLine().Append("# ").AppendLine(title);
-foreach (var item in starred)
+var starTask = Task.Run((async () =>
 {
-    mdStr.Append($"{idx}. [").Append(item.Name).Append("](").Append(item.HtmlUrl).Append(")\t[").Append(item.Description);
-    mdStr.Append(item.Description ?? "No description available.");
-    mdStr.Append("]\n");
-    idx++;
-}
-Console.WriteLine("文件内容:"+mdStr?.ToString() ?? "");
-// 写入到源码目录的md文件
-// 获取当前源码文件路径, 这个方法不确定可靠,依赖于项目编译输出目录结构
-var projectDir = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.Parent.FullName;
-var mdfile = Path.Combine(projectDir, "My Starts.md");
-Console.WriteLine("文件位置:" + mdfile);
-File.WriteAllText(mdfile, mdStr?.ToString() ?? ""
-                , new UTF8Encoding(false)); // UTF8默认包含Bom, 会在开头多个/FEFF标志,
-                                            // 影响md解析, 构造传false取消这个标志
 
-Console.WriteLine(mdStr?.ToString() ?? "");
+    var starred = await github.Activity.Starring.GetAllForUser(username);
+    var starsFile = Path.Combine(projectDir, "My Stars.md");
+    GenetResporityMarkdownFile(username, starred, starsFile);
+}));
+
+var respoTask = Task.Run((async () =>
+{
+
+    var resp = await github.Repository.GetAllForUser(username);
+    var RepositoriesFile = Path.Combine(projectDir, "My Repositories.md");
+    GenetResporityMarkdownFile(username, resp, RepositoriesFile);
+
+}));
+
+
+// 等待两个任务都完成
+await Task.WhenAll(starTask, respoTask);
+Console.WriteLine("两个任务都已完成");
+
+static void GenetResporityMarkdownFile(string username, IReadOnlyList<Repository> starred, string fileNameAndTitle)
+{
+    StringBuilder mdStr = new StringBuilder();
+    mdStr.AppendLine().Append("# ").AppendLine(fileNameAndTitle);// 标题
+
+    int idx = 1;
+    foreach (var item in starred)
+    {
+        mdStr.Append($"{idx}. [").Append(item.Name).Append("](").Append(item.HtmlUrl).Append(")\t[").Append(item.Description);
+        mdStr.Append(item.Description ?? "No description available.");
+        mdStr.Append("]\n");
+        idx++;
+    }
+    Console.WriteLine("文件内容:" + mdStr?.ToString() ?? "");
+    File.WriteAllText(fileNameAndTitle, mdStr?.ToString() ?? ""
+                    , new UTF8Encoding(false)); // UTF8默认包含Bom, 会在开头多个/FEFF标志,
+                                                // 影响md解析, 构造传false取消这个标志
+}
+
